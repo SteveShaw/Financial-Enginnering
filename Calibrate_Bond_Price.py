1a5e9f2372568c2349ef0a6d9025b248a4e6bc99
SteveShaw <steve.shao@yahoo.com>
2017-06-01 16:53:27 -0500

    Calibrate Lattice

--- Calibrate_Bond_Price.py
+++ Calibrate_Bond_Price.py
@@ -5,6 +5,39 @@ Created on Wed May 31 23:26:15 2017
 @author: SteveShaw
 """
 
+import numpy as np
 import BondPricing
+import scipy.optimize as sci_op
 
-def 
+N=15
+
+def RoundLattice(lattice, ndec=3):
+    round_lattice = []
+    for level in lattice:
+        round_lattice.append(np.around(level, decimals=ndec))
+    return round_lattice
+    
+def GenBDTShortRateLattice(a_guess, b, nper):
+    lattice = [np.array(a_guess)]
+    
+    for i in range(nper):
+        t = i + 1
+        v = np.arange(0, t+1) * b
+        v = np.exp(v)
+        lattice.append(v*a_guess)
+    
+    return lattice
+
+def GetZCBPrice( lattice ):
+    zcb_prcs = []
+    for level in lattice:
+       zcb_prcs.append(np.sum(level))
+    return np.array(zcb_prcs)
+#def MinLSESolver( initial, target, threshold ):
+a_guess= 0.05
+test_vr = GenBDTShortRateLattice(a_guess, 0.01, N-1)
+eps = BondPricing.ElementaryPrices(test_vr,N-1)
+#eps = RoundLattice(eps)        
+zcbs = GetZCBPrice(eps)
+
+#sci_op.leastsq()
